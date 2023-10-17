9882aaaed94826c4678217e6b1385d1935d83eb0
A1exAI00 <lksndrakinin@gmail.com>
2023-10-17 21:04:57 +0300

    Добавил код в source, добавил первый скрипт

---
+++ scripts/01-original_vs_simplified.jl
@@ -0,0 +1,77 @@
+#=
+Обучение упрощенного адаптивного осциллятора Хопфа (HAFO).
+Модель обучается на частоту внешнего сигнала
+=#
+
+include("../src/oscillator_models.jl")
+include("../src/time_series_tools.jl")
+include("../src/misc_tools.jl")
+
+using StaticArrays, CairoMakie
+
+########################################################################
+
+# Настройки генерируемого графика
+PLOT_RES = (1000, 800)
+PLOT_SAVING_DIR = "generated"; println(pwd())
+PLOT_FILENAME = "01-original_vs_simplified"
+PLOT_PX_PER_UNIT_PNG = 2
+
+########################################################################
+
+# Постоянные параметры системы
+γ, μ, ε, N = 1.0, 20.0, 10.0, 2 # 1.0, 1.0, 0.9, 2
+Ω_teach = [20.0, 160.0]
+A_teach = [1.0, 2.0]
+Φ_teach = [0.0, 0.0]
+system_param = SA[γ, μ, ε, N, Ω_teach..., A_teach..., Φ_teach...]
+
+# Начальные условия системы
+x₀, y₀, ω₀ = 1.0, 0.0, 40.0
+ω₀_1, ω₀_2 = 60.0, 85.0
+U₀_1 = SA[x₀, y₀, ω₀_1]
+U₀_2 = SA[x₀, y₀, ω₀_2]
+
+# Время интегрирования
+t₀, t₁ = 0.0, 150.0
+t_SPAN = [t₀, t₁]
+
+# Проверка параметров и начальных условий
+println("system_param = $system_param")
+#println("U₀ = $U₀")
+@assert length(Ω_teach) == N "length of `ω_teach` is not equal `N`"
+@assert length(A_teach) == N "length of `A_teach` is not equal `N`"
+@assert length(Φ_teach) == N "length of `Φ_teach` is not equal `N`"
+
+########################################################################
+
+# Интегрирование оригинальной системы 
+@time solution_o_1 = original_HAFO_integrate(U₀_1, t_SPAN, system_param)
+@time solution_o_2 = original_HAFO_integrate(U₀_2, t_SPAN, system_param)
+
+@time solution_s_1 = simplified_HAFO_integrate(U₀_1, t_SPAN, system_param)
+@time solution_s_2 = simplified_HAFO_integrate(U₀_2, t_SPAN, system_param)
+
+#t_sol_1 = solution.t
+
+########################################################################
+
+fig = Figure(resolution=PLOT_RES)
+ax_ω = Axis(fig[1,1], 
+    title="ω(t)",
+    xlabel="t",
+    ylabel="ω")
+
+hlines!(ax_ω, 0.0, color=:black)
+hlines!.(ax_ω, Ω_teach, color=:black, linestyle=:dot)
+
+lines!(ax_ω, solution_o_1.t, solution_o_1[3,:], label="original1")
+lines!(ax_ω, solution_o_2.t, solution_o_2[3,:], label="original2")
+lines!(ax_ω, solution_s_1.t, solution_s_1[3,:], label="simplified1")
+lines!(ax_ω, solution_s_2.t, solution_s_2[3,:], label="simplified2")
+
+axislegend(ax_ω)
+
+
+savingpath = joinpath(PLOT_SAVING_DIR, PLOT_FILENAME*"$(time_ns()).png")
+save(savingpath, fig, px_per_unit=PLOT_PX_PER_UNIT_PNG)
---
+++ src/oscillator_models.jl
@@ -0,0 +1,96 @@
+#=
+Базовые функции, необходимые для интегрирования модели 
+адаптивного осциллятора Хопфа (Стюарта-Ландау) и адаптивного 
+Central Pattern Generator (CPG) из статьи
+"A four-state adaptive Hopf oscillator"
+=#
+
+########################################################################
+
+#using Statistics: mean
+using OrdinaryDiffEq, StaticArrays, Interpolations
+include("../src/misc_tools.jl")
+
+########################################################################
+
+const RELTOL::Float64, ABSTOL::Float64 = 1e-5, 1e-5 # 1e-5, 1e-5
+const MAXITERS::Int64 = Int(1e7)
+
+# Different alg: 
+# Rodas5P - for stiff systems; faild on automatic differentiation
+# Tsit5 - 
+const ALG = Tsit5
+
+########################################################################
+
+function original_HAFO_model(u, p, t)
+    x, y, ω = u
+    γ, μ, ε, N = p[1:4]
+    N = Int(N)
+    Ω_teach = p[0N+5:1N+4]
+    A_teach = p[1N+5:2N+4]
+    Φ_teach = p[2N+5:3N+4]
+
+    r² = x^2 + y^2; r = sqrt(r²)
+    Fₜ = sum(A_teach.*cos.(Ω_teach .* t .+ Φ_teach))
+
+    return SA[
+        γ*(μ-r²)*x - ω*y + ε*Fₜ, 
+        γ*(μ-r²)*y + ω*x,
+        -ε*Fₜ*y/r]
+end
+
+function simplified_HAFO_model(u, p, t)
+    x, y, ω = u
+    γ, μ, ε, N = p[1:4]
+    N = Int(N)
+    Ω_teach = p[0N+5:1N+4]
+    A_teach = p[1N+5:2N+4]
+    Φ_teach = p[2N+5:3N+4]
+
+    r² = x^2 + y^2; #r = sqrt(r²)
+    Fₜ = sum(A_teach.*cos.(Ω_teach .* t .+ Φ_teach))
+
+    return SA[
+        γ*(μ-r²)*x - ω*y + ε*Fₜ, 
+        γ*(μ-r²)*y + ω*x,
+        -ε*Fₜ*y]
+end
+
+function four_state_model(u, p, t)
+    x, y, ω, α = u
+    γ, μ, ε, η, N = p[1:5]
+    N = Int(N)
+    Ω_teach = p[0N+6:1N+5]
+    A_teach = p[1N+6:2N+5]
+    Φ_teach = p[2N+6:3N+5]
+
+    r² = x^2 + y^2; #r = sqrt(r²)
+    Fₜ = sum([A_teach.*cos.(Ω_teach .* t .+ Φ_teach) for i in 1:N]) - α*x
+
+    return SA[
+        γ*(μ-r²)*x - ω*y + ε*Fₜ, 
+        γ*(μ-r²)*y + ω*x,
+        -ε*Fₜ*y,
+        η*Fₜ*x]
+end
+
+########################################################################
+
+function original_HAFO_integrate(U₀, t_span, param; reltol=RELTOL, abstol=ABSTOL, maxiters=MAXITERS, check_success=false)
+    prob = ODEProblem(original_HAFO_model, U₀, t_span, param)
+    sol = solve(prob, ALG(), reltol=reltol, abstol=abstol, maxiters=maxiters)
+    return (check_success && sol.retcode!=:Success) ? NaN : sol
+end
+
+function simplified_HAFO_integrate(U₀, t_span, param; reltol=RELTOL, abstol=ABSTOL, maxiters=MAXITERS, check_success=false)
+    prob = ODEProblem(simplified_HAFO_model, U₀, t_span, param)
+    sol = solve(prob, ALG(), reltol=reltol, abstol=abstol, maxiters=maxiters)
+    return (check_success && sol.retcode!=:Success) ? NaN : sol
+end
+
+function four_state_integrate(U₀, t_span, param; reltol=RELTOL, abstol=ABSTOL, maxiters=MAXITERS, check_success=false)
+    prob = ODEProblem(four_state_model, U₀, t_span, param)
+    sol = solve(prob, ALG(), reltol=reltol, abstol=abstol, maxiters=maxiters)
+    return (check_success && sol.retcode!=:Success) ? NaN : sol
+end
